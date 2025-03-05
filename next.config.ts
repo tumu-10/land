9d3bcf57e641c151682bc070d7cbd622d6fd4b1e
tumu-10 <81578497+tumu-10@users.noreply.github.com>
2025-03-05 13:58:41 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,16 @@
+// next.config.js
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  
-  
+  reactStrictMode: true,
+  sassOptions: {
+    implementation: 'sass', // Changed from object to string
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  images: {
+    domains: ['images.unsplash.com', 'localhost'],
+  }
 };
-
-
 
 export default nextConfig;
